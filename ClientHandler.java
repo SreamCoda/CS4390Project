eeef4a86ef35c368ec67c747b5e62da2b18ccc54
Mariana Lucio <marianaclucio@outlook.com>
2022-04-26 16:43:03 -0500

    Update ClientHandler.java

--- ClientHandler.java
+++ ClientHandler.java
@@ -3,44 +3,37 @@ import java.net.*;
 
 public class ClientHandler implements Runnable {
     private Socket client;
-    BufferedReader in;
-    PrintWriter out;
+    BufferedReader inFromClient;
+    PrintWriter outToClient;
 
+    //Constructor
     public ClientHandler(Socket clientSocket) throws IOException{
         this.client = clientSocket;
-        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        out = new PrintWriter(client.getOutputStream(), true);
+        inFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        outToClient = new PrintWriter(client.getOutputStream(), true);
     }
 
+    //Calculates basic math equations 
     @Override
     public void run() {
         try{
             while(true) { 
-                //This will read in and store the equation from the client
-                String clientEquations = in.readLine(); 
-
-                //this is where you'll calculate the basic equations.
-                //default variable for testing purposes, all it's doing rn is sending back the exact same message/equation to the client
+                //Reads in equation from client
+                String clientEquations = inFromClient.readLine(); 
                 String answer = clientEquations;
       
                 //this sends back the answer as a string to the client where it'll output it
-                out.println(answer.toString()); 
+                outToClient.println(answer.toString()); 
             }
           }
-          
           catch (IOException e){
               System.err.println("IO Exception in ClientHandler");
               System.err.println(e.getStackTrace());
+          } finally {
+            outToClient.close();
+
+            try { inFromClient.close(); } 
+            catch (IOException e) {e.printStackTrace();}
           }
-          finally {
-            out.close();
-            try {
-                in.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-          }
-        
     }
-    
 }
